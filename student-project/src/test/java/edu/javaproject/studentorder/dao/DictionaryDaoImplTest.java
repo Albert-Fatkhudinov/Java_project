84cb6c0eef07b23c6fce3ea19cd730a74c955d2b
Albert-Fatkhudinov <fasberg2@gmail.com>
2020-04-19 17:19:58 +0300

    create DictionaryDaoImplTest.java using JUNIT

---
+++ student-project/src/test/java/edu/javaproject/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,12 @@
+package edu.javaproject.studentorder.dao;
+
+
+import org.junit.Test;
+
+public class DictionaryDaoImplTest {
+
+    @Test
+    public void testExample() {
+        System.out.println("TEST");
+    }
+}
